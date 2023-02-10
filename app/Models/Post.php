1374bbb7d3ba9ea6aa314ae16ea7e8360541e89e
abdallah <abdallahelmekawy6@gmail.com>
2023-02-10 16:23:59 +0200

    eloquent post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,65 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    public $title;
-    public $slug;
-    public $date;
-    public $excerpt;
-    public $body;
-    public function __construct($title, $slug, $date, $excerpt, $body)
-    {
-        $this->title = $title;
-        $this->slug = $slug;
-        $this->date = $date;
-        $this->excerpt = $excerpt;
-        $this->body = $body;
-    }
-    public static function find($slug)
-    {
-        // if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-        //     throw new ModelNotFoundException;
-        // }
-        // $post = cache()->remember("post.{$slug}", now()->addMinutes(30), function () use ($path) {
-        //     var_dump('file_get_contents');
-        //     return file_get_contents($path);
-        // });
-        // return cache()->remember("post.{$slug}", 1200, fn () => file_get_contents($path));
-
-        $post = static::all()->firstWhere('slug', $slug);
-        return $post;
-    }
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-        if (!$post) {
-            throw new ModelNotFoundException;
-        }
-        return $post;
-    }
-    public static function all()
-    {
-        return cache()->rememberForever(
-            'posts.all',
-            fn () => collect(File::files(resource_path('posts')))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(
-                    fn ($document) => new Post(
-                        $document->title,
-                        $document->slug,
-                        $document->date,
-                        $document->excerpt,
-                        $document->body()
-                    )
-                )->sortByDesc('date')
-        );
-        // return array_map(fn ($post) => $post->getContents(), $posts);
-    }
+    use HasFactory;
 }
