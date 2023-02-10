44a7f89825f5b87ebde935859da65de8d8a91117
abdallah <abdallahelmekawy6@gmail.com>
2023-02-10 14:00:23 +0200

    blade components

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-
-<body>
-    <article>
-        <h1>{{$post->title}}</h1>
-        <p>{!!$post->body!!}</p>
-    </article>
-    <a href="/">Go back</a>
-</body>
-
-</html>
+<x-layout2>
+    <x-slot name="content">
+        <article>
+            <h1>{{ $post->title }}</h1>
+            <p>{!! $post->body !!}</p>
+        </article>
+        <a href="/">Go back</a>
+    </x-slot>
+</x-layout2>
