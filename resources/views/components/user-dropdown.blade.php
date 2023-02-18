6cda3b88af1dbecceb85a208e47f3e78dc63cc04
abdallah <abdallahelmekawy6@gmail.com>
2023-02-18 13:42:56 +0200

    hiding admin links

--- resources/views/components/user-dropdown.blade.php
+++ resources/views/components/user-dropdown.blade.php
@@ -5,8 +5,10 @@
                 {{ $slot }}
             </button>
         </x-slot>
-        <x-dropdown-item href="/admin/posts" :active="request()->is('admin/dashboard')">Dashboard</x-dropdown-item>
-        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+        @admin
+            <x-dropdown-item href="/admin/posts" :active="request()->is('admin/dashboard')">Dashboard</x-dropdown-item>
+            <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+        @endadmin
         <x-dropdown-item x-data={} @click.prevent="document.querySelector('#logout-form').submit()" href="#">Logout
         </x-dropdown-item>
         <form id="logout-form" action="/logout" method="POST" class="hidden">
--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,11 @@ Route::post('/login', [SessionsController::class, 'store'])->middleware('guest')
 
 Route::post('/newsteller', NewsletterController::class);
 
-
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('can:admin');
-Route::post('admin/posts/create', [AdminPostController::class, 'store'])->middleware('can:admin');
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('can:admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('can:admin');
-Route::patch('admin/posts/{post}/edit', [AdminPostController::class, 'update'])->middleware('can:admin');
-Route::delete('/admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('can:admin');
+Route::middleware('can:admin')->group(function () {
+    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+    Route::post('admin/posts/create', [AdminPostController::class, 'store']);
+    Route::get('admin/posts', [AdminPostController::class, 'index']);
+    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    Route::patch('admin/posts/{post}/edit', [AdminPostController::class, 'update']);
+    Route::delete('/admin/posts/{post}', [AdminPostController::class, 'destroy']);
+});
