e40abe202eebd856c94073fee656938c35360ed6
abdallah <abdallahelmekawy6@gmail.com>
2023-02-18 13:51:43 +0200

    refactoring

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -14,35 +14,36 @@ class AdminPostController extends Controller
         $posts =  Post::latest()->paginate(10);
         return view('admin.posts.index', ['posts' => $posts]);
     }
-    public function edit(Post $post)
-    {
-        return view('admin.posts.edit', ['post' => $post]);
-    }
-    public function update(Post $post)
-    {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'excerpt' => 'required',
-            'body' => 'required',
-            'thumbnail' => 'sometimes|image',
-            'category_id' => ['required', Rule::exists('categories', 'id')],
-        ]);
-        $attributes['slug'] = Str::slug($attributes['title']);
-        if (request()->has('thumbnail')) {
-            $path = request()->file('thumbnail')->store('thumbnails');
-            $attributes['thumbnail'] = $path;
-        }
-        $post->update($attributes);
-        return redirect('/posts/' . $post->slug)->with('success', 'Post has been updated.');
-    }
-
-
     public function create()
     {
         return view('admin.posts.create');
     }
     public function store()
     {
+        $attributes = static::validateRequest();
+        $attributes['user_id'] = auth()->user()->id;
+        $post = Post::create($attributes);
+        return redirect('/posts/' . $post->slug)->with('success', 'Post has been created.');
+    }
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', ['post' => $post]);
+    }
+    public function update(Post $post)
+    {
+        $attributes = static::validateRequest();
+        $post->update($attributes);
+        return redirect('/posts/' . $post->slug)->with('success', 'Post has been updated.');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/admin/posts')->with('success', 'Post deleted.');
+    }
+
+    private static function validateRequest()
+    {
         $attributes = request()->validate([
             'title' => 'required',
             'excerpt' => 'required',
@@ -50,20 +51,11 @@ class AdminPostController extends Controller
             'thumbnail' => 'sometimes|image',
             'category_id' => ['required', Rule::exists('categories', 'id')],
         ]);
-
+        $attributes['slug'] = Str::slug($attributes['title']);
         if (request()->has('thumbnail')) {
             $path = request()->file('thumbnail')->store('thumbnails');
             $attributes['thumbnail'] = $path;
         }
-
-        $attributes['slug'] = Str::slug($attributes['title']);
-        $attributes['user_id'] = auth()->user()->id;
-        $post = Post::create($attributes);
-        return redirect('/posts/' . $post->slug)->with('success', 'Post has been created.');
-    }
-    public function destroy(Post $post)
-    {
-        $post->delete();
-        return redirect('/admin/posts')->with('success', 'Post deleted.');
+        return $attributes;
     }
 }
